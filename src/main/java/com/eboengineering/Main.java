4c1a7b44f7133ac7057ec6c30189435f1d7297a1
Eugene <eugenebasiliscooca@gmail.com>
2025-05-02 14:54:52 +0800

    Task 2

---
+++ src/main/java/com/eboengineering/Main.java
@@ -0,0 +1,15 @@
+package com.eboengineering;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char a = 'H';
+        int b = 3110;
+        char c = 'w';
+        byte d = '0';
+        String e = "rld ";
+        float f = 2.0f;
+
+        System.out.println("" + a + b + " " + c + d + e + f);
+    }
+}
