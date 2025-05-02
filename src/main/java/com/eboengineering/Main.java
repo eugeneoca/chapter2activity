ed5f14905988d72df793ee0da9f66ea3338e5a24
Eugene <eugenebasiliscooca@gmail.com>
2025-05-02 15:10:45 +0800

    Fix task 2

--- src/main/java/com/eboengineering/Main.java
+++ src/main/java/com/eboengineering/Main.java
@@ -7,11 +7,11 @@ public class Main {
         char a = 'H';
         int b = 3110;
         char c = 'w';
-        byte d = '0';
+        short d = 0;
         String e = "rld ";
         float f = 2.0f;
 
-        System.out.println("" + a + b + " " + c + d + e + f);
+        System.out.println("" + a + b + " " + c + ""+d + e + f);
 
 
         TaskThree.run();
